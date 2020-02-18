58d8ad500376350b48f6eaf6d5a951d21ce43eea
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-02-18 19:48:04 +0500

    addEvent

--- script/script.js
+++ script/script.js
@@ -1,3 +1,31 @@
 'use strict';
 
-const startButton = document.querySelector('.start-button');
+const startButton = document.querySelector('.start-button'),
+    firstScreen = document.querySelector('.first-screen'),
+    mainForm = document.querySelector('.main-form'),
+    formCalculate = document.querySelector('.form-calculate'),
+    endButton = document.querySelector('.end-button'),
+    total = document.querySelector('.total');
+
+
+const showElem = (elem) => {
+    elem.style.display = 'block';
+};
+
+const hideElem = (elem) => {
+    elem.style.display = 'none';
+};
+
+startButton.addEventListener('click', () => {
+ showElem(mainForm);
+ hideElem(firstScreen);
+});
+
+endButton.addEventListener('click', () => {
+    for(let elem of formCalculate.elements){
+        if (elem.tagName === 'FIELDSET'){
+            hideElem(elem);
+        }
+    }
+});
+

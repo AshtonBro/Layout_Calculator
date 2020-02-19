ade636d15be15cf23223db71b2618f07f070744b
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-02-19 20:59:34 +0500

    new func priceCulc

--- script/script.js
+++ script/script.js
@@ -7,8 +7,26 @@ const startButton = document.querySelector('.start-button'),
     endButton = document.querySelector('.end-button'),
     total = document.querySelector('.total'),
     fastRange = document.querySelector('.fast-range'),
-    subTotal = document.querySelector('.subtotal');
+    subTotal = document.querySelector('.subtotal'),
+    totalPriceSum = document.querySelector('.total_price__sum');
 
+const DATA = {
+    whichSite: ["landing", "multiPage", "onlineStore"],
+    price: [4000, 8000, 26000],
+    desktopTemplates: [50, 40, 30],
+    adapt: 20,
+    mobileTemplates: 15,
+    editable: 10,
+    metrikaYandex: [500, 1000, 2000],
+    analyticsGoogle: [850, 1350, 3000],
+    sendOrder: 500,
+    deadlineDay: [
+        [2, 7],
+        [3, 10],
+        [7, 14]
+    ],
+    deadlinePercent: [20, 17, 15]
+};
 
 const showElem = (elem) => {
     elem.style.display = 'block';
@@ -18,23 +36,62 @@ const hideElem = (elem) => {
     elem.style.display = 'none';
 };
 
+const priceCulc = (elem) => {
+    let result = 0,
+        index = 0,
+        options = [];
+
+    if (elem.name === 'whichSite') {
+        for (const item of formCalculate.elements) {
+            if (item.type === 'checkbox') {
+                item.checked = false;
+            }
+        }
+        hideElem(fastRange);
+    }
+
+    for (const item of formCalculate.elements) {
+        if (item.name === 'whichSite' && item.checked) {
+            index = DATA.whichSite.indexOf(item.value);
+        } else if (item.classList.contains('calc-handler') && item.checked) {
+            options.push(item.value);
+        }
+    }
+
+    options.forEach((key) => {
+        if (typeof(DATA[key]) === 'number') {
+            if (key === 'sendOrder') {
+                result += DATA[key];
+            } else {
+                result += DATA.price[index] * DATA[key] / 100;
+            }
+        }
+    });
+
+    result += DATA.price[index];
+
+
+    totalPriceSum.textContent = result;
+};
 const handlerCallBackForm = (event) => {
     const target = event.target;
-    
-    if (target.classList.contains('want-faster')){
-       
+
+    if (target.classList.contains('want-faster')) {
         target.checked ? showElem(fastRange) : hideElem(fastRange);
+    }
+    if (target.classList.contains('calc-handler')) {
+        priceCulc(target);
     }
 };
 
 startButton.addEventListener('click', () => {
- showElem(mainForm);
- hideElem(firstScreen);
+    showElem(mainForm);
+    hideElem(firstScreen);
 });
 
 endButton.addEventListener('click', () => {
-    for(let elem of formCalculate.elements){
-        if (elem.tagName === 'FIELDSET'){
+    for (let elem of formCalculate.elements) {
+        if (elem.tagName === 'FIELDSET') {
             hideElem(elem);
         }
     }
